4b2dc7279526605201318909069b5a65603916ad
Lev Zurcher <lazurcher5@gmail.com>
2026-05-12 20:04:05 -0600

    Created HTML, CSS, and JS files
    index.html includes code for a div called "map".
    map.js created to set map location to BYU-I coordinates.
    MeecroWaveh.css scales map height.
    
    Bound to be changed, especially with Tailwind CSS being included.

---
+++ js_files/map.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([43.819046, -111.783228], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//DEBUG
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
