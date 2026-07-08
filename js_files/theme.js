a647b8b0e222583d344b7c97a17e61e885ad37d3
wyman22002 <tannerw2002@hotmail.com>
2026-07-08 14:47:10 -0600

    Made dark mode colors consitstant

--- js_files/theme.js
+++ js_files/theme.js
@@ -1,13 +1,23 @@
 
-
 const toggleBtn = document.getElementById("theme");
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme == "dark") {
+    document.body.classList.add("dark");
+}
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
+    let theme = "light"
+
     if (document.body.classList.contains("dark")) {
         toggleBtn.textContent = "Light Mode";
+        theme = "dark"
     } else {
         toggleBtn.textContent = "Dark Mode";
+        theme = "light"
     }
+
+    localStorage.setItem("theme", theme)
 });
