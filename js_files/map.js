d1daeec1c2344981cacc51b1c3d1f009cc40ac34
Lev Zurcher <lazurcher5@gmail.com>
2026-07-08 16:01:44 -0600

    Update map.js
    Re-implemented code so that all map markers contain building names.
    Also, live user location is enabled!

--- js_files/map.js
+++ js_files/map.js
@@ -402,6 +402,14 @@ map.on('zoomend', function() {
             map.removeControl(currentControl);
             currentControl = null;
         }
+
+        if (currentZoom <= 14) {
+            displayZoomMessage(true);
+        } else {
+            displayZoomMessage(false);
+        }
+    } else {
+        displayZoomMessage(false);
     }
 });
 
@@ -490,8 +498,10 @@ function displayZoomedOut(load_it) {
                 </ul>
             </div>
         `;
- 
-        const marker = L.marker(latlng).addTo(map);
+        
+        const icon_html = '<p class="building-icon-html">' + building.building_name + '</p>';
+        var building_icon = L.divIcon({className: 'leaflet-building-icon', html: icon_html});
+        const marker = L.marker(latlng, {icon: building_icon}).addTo(map);
         marker.building_id = building.building_id;
  
         marker.bindTooltip(tooltipContent, {
@@ -524,3 +534,78 @@ window.addEventListener('DOMContentLoaded', () => {
     populateFloorPins();
     displayZoomedOut(true);
 });
+
+//Zooms in to user position #MAY USE FOR A BUTTON
+// map.locate({setView: true, maxZoom: 16});
+
+/** Function: Displays "Please zoom in" if zoomed out way too far */
+function displayZoomMessage(load_it) {
+    if (load_it) {
+        if (!document.getElementById('zoom_element')) {
+            const zoom_element = document.createElement("div");
+            zoom_element.className = "zoom-container";
+            zoom_element.id = 'zoom_element';
+            
+            zoom_element.innerHTML = "<p class='zoom-icon-html'>Please zoom in</p>"
+            
+            const mapDOM = document.getElementById('map');
+            mapDOM.append(zoom_element);
+        }
+    } else {
+        try {
+            if (document.getElementById('zoom_element'))
+                document.getElementById('zoom_element').remove();
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+/************************************************
+ * Tracks user position and draws a circle at that position
+ **************************************************/
+
+// Declare variables to store marker and accuracy circle
+let userMarker;
+let userCircle;
+
+// Define the success handler for when a location is found
+function onLocationFound(e) {
+    const radius = e.accuracy/2;
+
+    // If the marker doesn't exist yet, create it
+    if (!userMarker) {
+        // Sets the size of the icon
+        const gpsIconSize = [16, 16]
+
+        var gpsIcon = L.icon({
+            iconUrl: 'images/gps_icon.png',
+            iconSize: gpsIconSize,
+            iconAnchor: gpsIconSize/2,
+            popupAnchor: [-3,-76]
+        });
+        userMarker = L.marker(e.latlng, {icon: gpsIcon}).addTo(map);
+
+        userCircle = L.circle(e.latlng, radius).addTo(map);
+    } else {
+        // If it already exists, simply move it to the new coordinates
+        userMarker.setLatLng(e.latlng);
+        userCircle.setLatLng(e.latlng);
+        userCircle.setRadius(radius);
+    }
+}
+
+// Define the error handler if geolocation is denied or fails
+function onLocationError(e) {
+    alert(e.message);
+}
+
+// Attach the event listeners to the map
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+// Start continuous tracking
+map.locate({
+    watch:true,         // Continuous watching of the user's position
+    enableHighAccuracy:true // Forces the browser to seek a precise GPS fix
+})
